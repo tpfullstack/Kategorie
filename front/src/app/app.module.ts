e222cace90a1d715940e6e0048bec0e34d3d9ddb
KKarim <karim.kassa@univ-rouen.fr>
2024-12-06 20:51:59 +0100

    Revert "build(core): update Angular framework to version 18"
    This reverts commit 4dd073c631d8121bbee902c910d10cb06ce6a750.

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms'; 
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 // Components
 import { AppComponent } from './app.component';
 import { CategoriesComponent } from './Components/categories/categories.component';
@@ -15,22 +15,28 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoaderInterceptor } from './core/helpers/loader.interceptor';
 
-@NgModule({ declarations: [
-        AppComponent,
-        CategoriesComponent,
-        AddCategorieComponent,
-        DetailsCategorieComponent,
-        UpdateCategorieComponent,
-        HeaderComponent
-    ],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        AppRoutingModule,
-        ReactiveFormsModule,
-        FormsModule,
-        NgxSpinnerModule,
-        BrowserAnimationsModule], providers: [
-        { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true },
-        provideHttpClient(withInterceptorsFromDi())
-    ] })
+@NgModule({
+  declarations: [
+    AppComponent,
+    CategoriesComponent,
+    AddCategorieComponent,
+    DetailsCategorieComponent,
+    UpdateCategorieComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule, 
+    NgxSpinnerModule,
+    BrowserAnimationsModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true }
+  ],
+  bootstrap: [AppComponent]
+})
 export class AppModule {}
--- front/src/app/core/helpers/loader.interceptor.ts
+++ front/src/app/core/helpers/loader.interceptor.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 import { NgxSpinnerService } from 'ngx-spinner';
